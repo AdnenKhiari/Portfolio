c729213f283465b471f0d9a6522b1943afca261a
AdnenKhiari <adnenkhiari484@gmail.com>
2024-01-02 23:28:29 +0100

    solved project server bug

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,13 +1,7 @@
-import parse from 'html-react-parser';
-import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { getDocFromFirestore, getPicUrl } from '@/app/utils'
-import { useParams } from "next/navigation";
-import { Project } from '../(components)/ProjectCard'
-import Loading from '@/app/(sharedComponents)/TechStackIcons/loading'
 import "./style.css"
-import { motion } from 'framer-motion';
-import { Metadata, ResolvingMetadata } from 'next';
+import { Metadata } from 'next';
 import ProjectDetails from './(components)/ProjectDetails';
 import { Timestamp } from 'firebase/firestore';
 
@@ -44,7 +38,6 @@ export default async function ProjectView({params}: any)  {
   const projectId = params.projectId
   const data = await getDocFromFirestore("projects/"+projectId)
   const img = await getPicUrl("PROJECTS/"+data.url)
-
   data.projectDate = new Timestamp(data.projectDate.seconds,data.projectDate.nanoseconds).toDate()
   data.lastUpdateDate = new Timestamp(data.lastUpdateDate.seconds,data.lastUpdateDate.nanoseconds).toDate()
 
